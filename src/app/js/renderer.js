a8646f056a1d21dbc56a0a38d5a145027a80480d
geninhocell <geninhocell@gmail.com>
2021-10-26 03:45:36 -0300

    add: styles about

--- src/app/js/renderer.js
+++ src/app/js/renderer.js
@@ -4,9 +4,9 @@ linkAbout.addEventListener('click', function(){
     window.api.send("open-window-about", "some data");
 });
 
-window.api.receive("fromMain", (data) => {
-  console.log(`Received ${JSON.stringify(data)} from main process`);
-});
+// window.api.receive("fromMain", (data) => {
+//   console.log(`Received ${JSON.stringify(data)} from main process`);
+// });
 
-window.api.send("toMain", "some data");
+// window.api.send("toMain", "some data");
 
